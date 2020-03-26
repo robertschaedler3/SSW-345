e2da39b5c250ca117c1e8282e982a7e0c6ceed08
Robert Schaedler III <robertschaedler3@gmail.com>
2020-03-26 12:52:25 -0400

    add github access token to .env

--- Homework4-REST/index.js
+++ Homework4-REST/index.js
@@ -1,29 +1,29 @@
 var request = require('request');
 
-const chalk  = require('chalk');
+const chalk = require('chalk');
 
 var urlRoot = "https://api.github.com";
 // NCSU Enterprise endpoint:
 //var urlRoot = "https://api.github.ncsu.edu";
 
-var userId = "yy2111";
+var userId = "robertschaedler3";
 var config = {};
+
 // Retrieve our api token from the environment variables.
+require('dotenv').config();
 config.token = process.env.GITHUBTOKEN;
 
-if( !config.token )
-{
+if (!config.token) {
 	console.log(chalk`{red.bold GITHUBTOKEN is not defined!}`);
 	console.log(`Please set your environment variables with appropriate token.`);
 	console.log(chalk`{italic You may need to refresh your shell in order for your changes to take place.}`);
 	process.exit(1);
 }
 
-console.log(chalk.green(`Your token is: ${config.token.substring(0,4)}...`));
+console.log(chalk.green(`Your token is: ${config.token.substring(0, 4)}...`));
 
 
-if (process.env.NODE_ENV != 'test')
-{
+if (process.env.NODE_ENV != 'test') {
 	(async () => {
 		await listAuthenicatedUserRepos();
 		//await listBranches(userId, "your repo");
@@ -34,8 +34,7 @@ if (process.env.NODE_ENV != 'test')
 	})()
 }
 
-function getDefaultOptions(endpoint, method)
-{
+function getDefaultOptions(endpoint, method) {
 	var options = {
 		url: urlRoot + endpoint,
 		method: method,
@@ -48,112 +47,97 @@ function getDefaultOptions(endpoint, method)
 	return options;
 }
 
-async function getUser()
-{
+async function getUser() {
 	let options = getDefaultOptions("/user", "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
+	return new Promise(function (resolve, reject) {
 		request(options, function (error, response, body) {
 
-			resolve( JSON.parse(body).login );
+			resolve(JSON.parse(body).login);
 		});
 	});
 }
 
-function listAuthenicatedUserRepos()
-{
+function listAuthenicatedUserRepos() {
 	let options = getDefaultOptions("/user/repos?visibility=all", "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
-		request(options, function (error, response, body) 
-		{
-			if( error )
-			{
-				console.log( chalk.red( error ));
+	return new Promise(function (resolve, reject) {
+		request(options, function (error, response, body) {
+			if (error) {
+				console.log(chalk.red(error));
 				reject(error);
 				return; // Terminate execution.
 			}
 
 			var obj = JSON.parse(body);
-			for( var i = 0; i < obj.length; i++ )
-			{
+			for (var i = 0; i < obj.length; i++) {
 				var name = obj[i].name;
-				console.log( name );
+				console.log(name);
 			}
 
 			// Return object for people calling our method.
-			resolve( obj );
+			resolve(obj);
 		});
 	});
 
 }
 
 // 1. Write code for listBranches in a given repo under an owner. See list branches
-async function listBranches(owner,repo)
-{
+async function listBranches(owner, repo) {
 	let options = getDefaultOptions(`/`, "GET");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
+	return new Promise(function (resolve, reject) {
 		request(options, function (error, response, body) {
 
 			// console.debug( options );
-			resolve( JSON.parse(body) );
+			resolve(JSON.parse(body));
 
 		});
 	});
 }
 
 // 2. Write code to create a new repo
-async function createRepo(owner,repo)
-{
+async function createRepo(owner, repo) {
 	let options = getDefaultOptions("/", "POST");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
+	return new Promise(function (resolve, reject) {
 		request(options, function (error, response, body) {
 
-			resolve( response.statusCode );
+			resolve(response.statusCode);
 
 		});
 	});
 
 }
 // 3. Write code for creating an issue for an existing repo.
-async function createIssue(owner,repo, issueName, issueBody)
-{
+async function createIssue(owner, repo, issueName, issueBody) {
 	let options = getDefaultOptions("/", "POST");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
+	return new Promise(function (resolve, reject) {
 		request(options, function (error, response, body) {
 
-			resolve( response.statusCode );
+			resolve(response.statusCode);
 
 		});
 	});
 }
 
 // 4. Write code for editing a repo to enable wiki support.
-async function enableWikiSupport(owner,repo)
-{
+async function enableWikiSupport(owner, repo) {
 	let options = getDefaultOptions("/", "PATCH");
 
 	// Send a http request to url and specify a callback that will be called upon its return.
-	return new Promise(function(resolve, reject)
-	{
+	return new Promise(function (resolve, reject) {
 		request(options, function (error, response, body) {
 
-			resolve( JSON.parse(body) );
+			resolve(JSON.parse(body));
 		});
-	});	
+	});
 }
 
 module.exports.getUser = getUser;
