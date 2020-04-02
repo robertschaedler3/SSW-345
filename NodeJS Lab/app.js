87b8ca0f8dbeed24d36c73849aaed2d10ea99dde
Robert Schaedler III <robertschaedler3@gmail.com>
2020-04-02 13:49:01 -0400

    update app.js

---
+++ NodeJS Lab/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3002;
+const app = express();
+
+const routes = require('./routes/routes');
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+app.get('/', (request, response) => {
+    response.send({
+        message: 'Node.js and Express REST API'
+    }
+    );
+});
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
+
+routes(app);
